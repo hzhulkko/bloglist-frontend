8eccda72c841b8193ee0a1f1ec6a48e25ece921b
Heidi Hulkko <hulkhei@gmail.com>
2020-03-29 22:20:38 +0300

    Reformat Notification

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Notification = ({notification}) => {
-    return (
-      <div className={notification.className}>
-        {notification.message}
-      </div>
-    )
-  }
+const Notification = ({ notification }) => {
+  return (
+    <div className={notification.className}>
+      {notification.message}
+    </div>
+  )
+}
 
-  export default Notification
+export default Notification
