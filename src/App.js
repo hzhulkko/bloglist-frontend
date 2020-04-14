e3cafbb3900ae118d9990da6626874ebeb9be27a
Heidi Hulkko <hulkhei@gmail.com>
2020-04-14 14:44:48 +0300

    Add navigation and links

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React from 'react'
 import Notification from './components/Notification'
 import Header from './components/Header'
 import BlogView from './components/BlogView'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from 'react-router-dom'
+import UserList from './components/UserList'
 
 const App = () => {
 
+  const padding = { padding: 5 }
+
   return (
     <div>
-      <Notification/>
-      <Header/>
-      <BlogView/>
+      <Router>
+        <div>
+          <Link style={padding} to="/">blogs</Link>
+          <Link style={padding} to="/users">users</Link>
+        </div>
+        <Notification/>
+        <Header/>
+        <Switch>
+          <Route path='/users'>
+            <UserList/>
+          </Route>
+          <Route path='/'>
+            <BlogView/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
