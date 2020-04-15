fe591d36c223c6b78ee50c7df82dd66b00de0ac1
Heidi Hulkko <hulkhei@gmail.com>
2020-04-15 10:40:18 +0300

    Render UserList when users are loaded or user has changed

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -6,22 +6,30 @@ const UserList = () => {
 
   const dispatch = useDispatch()
   const users = useSelector(state => state.users)
+  const currentUser = useSelector(state => state.user)
 
   useEffect(() => {
     dispatch(getAll())
-  }, [dispatch])
+  }, [dispatch, currentUser])
 
+  if (!currentUser || users.length === 0) {
+    return null
+  }
   return(
     <div id='user-list'>
       <h3>Users</h3>
       <table>
-        <tr><th>User</th><th>Blogs added</th></tr>
-        {users.map(user =>
-          <tr key={user.id}>
-            <td>{user.name}</td>
-            <td>{user.blogs.length}</td>
-          </tr>
-        )}
+        <thead>
+          <tr><th>User</th><th>Blogs added</th></tr>
+        </thead>
+        <tbody>
+          {users.map(user =>
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.blogs.length}</td>
+            </tr>
+          )}
+        </tbody>
       </table>
     </div>
   )
